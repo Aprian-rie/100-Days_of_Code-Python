e6f8743cba8088f2b4ceb3ce92728cf960d5ed77
Aprian Bwire <aprythomas5@gmail.com>
2023-07-27 20:34:53 +0300

    End of Day 1

--- day-1/bandsugester.py
+++ day-1/bandsugester.py
@@ -1,1 +1,11 @@
-print("Hello World")
+#1. Create a greeting for your program.
+print("Hello Comrade :) Welcome to my Band generator")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What city you grew up in ?\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("Name of your favourite pet\n")
+#4. Combine the name of their city and pet and show them their band name.
+band_name = city +" "+ pet_name
+#5. Make sure the input cursor shows on a new line:
+print("Your band name could be "+band_name)
