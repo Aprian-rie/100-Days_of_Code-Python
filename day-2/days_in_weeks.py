7d8aa2bbc38b81449255e58ede28a0f218c3a819
Aprian Bwire <aprythomas5@gmail.com>
2023-07-28 18:38:24 +0300

    days in weeks

---
+++ day-2/days_in_weeks.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇👇
+years_left = 90 - int(age)
+days_left = (years_left) * (365)
+weeks_left = (years_left) * (52)
+months_left = (years_left) * 12
+print(f"You have {days_left} days, {weeks_left} weeks, and {months_left} months left.")
+
+
+
+
