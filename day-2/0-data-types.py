50df8415cf5768589a64365a69ad885c03e2e21e
Aprian Bwire <aprythomas5@gmail.com>
2023-07-28 09:53:51 +0300

    type conversion

---
+++ day-2/0-data-types.py
@@ -0,0 +1,18 @@
+#Data Types
+
+#String
+
+print("Hello"[0])
+
+#Integer
+
+print(123 + 345)
+
+#Float
+3.14159
+
+#Boolean 
+True
+False
+
+
---
+++ day-2/1-type_check.py
@@ -0,0 +1,4 @@
+name = input("What is your name")
+num_name = len(name)
+str_num_name = str(num_name)
+print("Your name has "+ str_num_name + " characters")
