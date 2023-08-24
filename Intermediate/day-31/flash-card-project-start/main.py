ea2a34da8942a4aa9f359e92a235c24127c6391a
Apryan27 <aprythomas5@gmail.com>
2023-08-24 15:04:17 +0300

    flashcard complete

--- Intermediate/day-31/flash-card-project-start/main.py
+++ Intermediate/day-31/flash-card-project-start/main.py
@@ -4,53 +4,71 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 # ------- READING DATA FROM CSV FILE -------------------
-words_data = pandas.read_csv('data/french_words.csv')
-french_words = words_data['French']
-french_words_list = french_words.to_list()
+try:
+    words_data = pandas.read_csv('data/Words to learn.csv')
+except FileNotFoundError:
+    og_words_data = pandas.read_csv("data/french_words.csv")
+    to_learn = og_words_data.to_dict(orient="records")
+else:
+    to_learn = words_data.to_dict(orient="records")
+
 
 
 # print(french_words_list)
 
 # --------- Generating random french word ---------------
-def generate_random_words():
-    canvas.delete("french_tag")
-    canvas.delete("english_tag")
-    random_french_word = random.choice(french_words_list)
-    english_word_corr = words_data['English'][words_data.French == random_french_word]
-    canvas.create_text(380, 250, text=random_french_word, fill="black", font='Helvetica 35 bold', tags="french_tag")
-    window.after(3000, display_english(english_word_corr))
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_image)
+    flip_timer = window.after(3000, func=flip_card)
 
 
-def display_english(english_word):
-    canvas.create_text(380, 250, text=english_word, fill="black", font='Helvetica 35 bold', tags="english_tag")
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_image)
 
 
-# def timer():
-#     window.after(3000, )
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/Words to learn.csv", index=False)
+    next_card()
+
+
 # ----------------- UI SETUP ----------------------------
 window = Tk()
 window.title('FlashCardApp')
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
+flip_timer = window.after(3000, func=flip_card)
+
 canvas = Canvas(width=800, height=530)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_image = PhotoImage(file="images/card_front.png")
 card_back_image = PhotoImage(file="images/card_back.png")
-canvas.create_image(400, 263, image=card_front_image)
-canvas.create_text(380, 150, text="French", fill="black", font='Helvetica 25 italic')
-# canvas.create_text(380, 150, text="French", fill="black", font='Helvetica 25 italic')
+card_background = canvas.create_image(400, 263, image=card_front_image)
+card_title = canvas.create_text(380, 150, text="Title", fill="black", font='Ariel 25 italic')
+card_word = canvas.create_text(380, 250, text="Word", fill="black", font='Ariel 35 bold')
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
-cross_button = Button(command=generate_random_words)
+cross_button = Button(command=next_card)
 cross_image = PhotoImage(file="images/wrong.png")
 cross_button.config(image=cross_image, highlightthickness=0)
 cross_button.grid(row=1, column=0)
 
-tick_button = Button(command=generate_random_words)
+tick_button = Button(command=is_known)
 tick_image = PhotoImage(file="images/right.png")
 tick_button.config(image=tick_image, highlightthickness=0)
 tick_button.grid(row=1, column=1)
 
+next_card()
 window.mainloop()
