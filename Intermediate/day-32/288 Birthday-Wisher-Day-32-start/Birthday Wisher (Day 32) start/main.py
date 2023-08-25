67591a9f61243e1bf337736237aeab408b8c2b7a
Apryan27 <aprythomas5@gmail.com>
2023-08-26 00:01:57 +0300

    sent email succesfully added port

--- Intermediate/day-32/288 Birthday-Wisher-Day-32-start/Birthday Wisher (Day 32) start/main.py
+++ Intermediate/day-32/288 Birthday-Wisher-Day-32-start/Birthday Wisher (Day 32) start/main.py
@@ -3,7 +3,7 @@ import smtplib
 my_email = "aprianbwire@gmail.com"
 password = "aqgfvwixiqgszabj"
 
-connection = smtplib.SMTP("smtp.gmail.com")
+connection = smtplib.SMTP("smtp.gmail.com", 587)
 connection.starttls()
 connection.login(user=my_email, password=password)
 connection.sendmail(from_addr=my_email, to_addrs="aprythomas5@gmail.com", msg="Hello Aprian")
