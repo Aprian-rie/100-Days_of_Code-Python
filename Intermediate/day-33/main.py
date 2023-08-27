5f7034b4943c88d9fe8c65892c8fe5d63292fd32
Apryan27 <aprythomas5@gmail.com>
2023-08-27 22:24:12 +0300

    learnt about passing paremters in an API

--- Intermediate/day-33/main.py
+++ Intermediate/day-33/main.py
@@ -1,10 +1,31 @@
 import requests
+from datetime import datetime
+LAT = -6.183078
+LONG = 35.748161
+#
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# longitude = response.json()['iss_position']['longitude']
+# latitude = response.json()['iss_position']['latitude']
+#
+# position = (longitude, latitude)
+# print(position)
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+parameters = {
+    "lat": LAT,
+    "lng": LONG,
+    "formatted": 0,
+}
+
+response = requests.get(url=" https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
+data = response.json()
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
 
-longitude = response.json()['iss_position']['longitude']
-latitude = response.json()['iss_position']['latitude']
+print(sunrise)
+print(sunset)
 
-position = (longitude, latitude)
-print(position)
+time_now = datetime.now()
+print(time_now.hour)
