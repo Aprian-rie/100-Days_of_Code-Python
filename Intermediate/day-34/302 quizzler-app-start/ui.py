ebb0d8fb45d9cd83978245dd123cfb8d0357d90e
Apryan27 <aprythomas5@gmail.com>
2023-08-27 23:35:30 +0300

    creating UI

--- Intermediate/day-34/302 quizzler-app-start/ui.py
+++ Intermediate/day-34/302 quizzler-app-start/ui.py
@@ -1,2 +1,10 @@
+from tkinter import *
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("AprieQuiz")
+        self.
+
+        self.window.mainloop()
