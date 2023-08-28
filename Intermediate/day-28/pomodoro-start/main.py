641b2e2136ae6098338afb8ba3ab698d1879ef69
Apryan27 <aprythomas5@gmail.com>
2023-08-28 15:51:47 +0300

    day 35 end pt 2

--- Intermediate/day-28/pomodoro-start/main.py
+++ Intermediate/day-28/pomodoro-start/main.py
@@ -34,7 +34,6 @@ def start_timer():
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-
     if reps % 8 == 0:
         count_down(long_break_sec)
         timer_label.config(text="Long Break", fg=RED)
